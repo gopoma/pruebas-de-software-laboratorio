2b0ffcca9b08ef61066faa0bf7e6d626762cb738
Your Name <dpacoria@unsa.edu.pe>
2024-05-10 22:34:19 -0500

    Se colocó los casos de prueba y se corrigió el codigo

--- laboratorio01/Calculadora.py
+++ laboratorio01/Calculadora.py
@@ -8,6 +8,10 @@ def multiplicar(x: float, y: float) -> float:
     return x * y
 
 def dividir(x: float, y: float) -> float:
+
+    # Corrección al caso de prueba #6
+    if y==0:
+        return "No se puede dividir entre 0"
     return x / y
 
 def potenciar(x: float, y: float) -> float:
@@ -16,8 +20,12 @@ def potenciar(x: float, y: float) -> float:
 def calcular(operacion: str, numeros: list[float]) -> float | str:
     if len(numeros) != 2:
         return "¡Se necesitan 2 números para la Calculadora!"
-    
+
     numero1, numero2 = numeros
+
+    # Correción al caso de prueba #12
+    if isinstance(numero1, str) or isinstance(numero2, str):
+        return "Los valores ingresados deben ser números"
     
     if operacion == "+":
         return sumar(numero1, numero2)
@@ -34,8 +42,63 @@ def calcular(operacion: str, numeros: list[float]) -> float | str:
 
     assert False
 
-assert calcular("+", [1.5, 3]) == 4.5
-assert calcular("-", [3, 10]) == -7
-assert calcular("*", [3, 4]) == 12
-assert calcular("/", [4.5, 3]) == 1.5
-assert calcular("**", [2, 10]) == 1024
+# Caso de Prueba #1.
+assert calcular('+', [1, 5]) == 6
+
+# Caso de Prueba #2.
+assert calcular('-', [9, 8]) == 1
+
+# Caso de Prueba #3
+assert calcular('*', [10, 5]) == 50
+
+# Caso de Prueba #4
+assert calcular('**', [2, 5]) == 32
+
+# Caso de Prueba #5
+assert calcular('/', [10, 5]) == 2
+
+# Caso de Prueba #6
+assert calcular('/', [10, 0]) == "No se puede dividir entre 0"
+
+# Caso de Prueba #7
+assert calcular('+', [105.2, 53.8]) == 159
+
+# Caso de Prueba #8
+assert calcular('-', [10.5, -13.2]) == 23.7
+
+# Caso de Prueba #9
+assert calcular('*', [10.5, 0.5]) == 5.25
+
+# Caso de Prueba #10
+assert calcular('/', [97.25, 52.64]) == 1.847454407294833
+
+# Caso de Prueba #11
+assert calcular('**', [64, 0.5]) == 8
+
+# Caso de Prueba #12
+assert calcular('+', ["saludos", 5]) == "Los valores ingresados deben ser números"
+assert calcular('-', [5, "saludos"]) == "Los valores ingresados deben ser números"
+
+# Caso de Prueba #13
+assert calcular('+', [115, 0]) == 115
+
+# Caso de Prueba #14
+assert calcular('*', [115, 0]) == 0
+
+# Caso de Prueba #15
+assert calcular('*', [115.25, 1]) == 115.25
+
+# Caso de Prueba #16
+assert calcular('**', [10, 1]) == 10
+
+# Caso de Prueba #17
+assert calcular('**', [115, 0]) == 1
+
+# Caso de Prueba #18
+assert calcular('**', [64, -0.5]) == 0.125
+
+# Caso de Prueba #19
+assert calcular('*/', [115, 0]) == "¡Operación inválida!"
+
+# Caso de Prueba #20
+assert calcular('+', [0]) == "¡Se necesitan 2 números para la Calculadora!"
