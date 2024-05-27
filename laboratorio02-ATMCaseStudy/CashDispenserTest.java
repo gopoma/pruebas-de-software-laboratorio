b747434968dfdfe40f53ff807c522bba2907236b
gopoma <gordono@unsa.edu.pe>
2024-05-27 18:52:56 -0500

    Se implementaron las Pruebas Unitarias de los componentes del ATM

---
+++ laboratorio02-ATMCaseStudy/CashDispenserTest.java
@@ -0,0 +1,45 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * The test class CashDispenserTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CashDispenserTest
+{
+    public final static int CASH_DISPENSER_INITIAL_COUNT = 500;
+
+    @Test
+    public void sufficientCashAvailableValido() {
+        CashDispenser cd = new CashDispenser();
+        
+        assertTrue(cd.isSufficientCashAvailable(1 * 20));
+        assertTrue(cd.isSufficientCashAvailable(10 * 20));
+        assertTrue(cd.isSufficientCashAvailable(100 * 20));
+        assertTrue(cd.isSufficientCashAvailable(200 * 20));
+        assertTrue(cd.isSufficientCashAvailable(300 * 20));
+        assertTrue(cd.isSufficientCashAvailable(400 * 20));
+        assertTrue(cd.isSufficientCashAvailable(499 * 20));
+        assertTrue(cd.isSufficientCashAvailable(CASH_DISPENSER_INITIAL_COUNT * 20));
+    }
+    
+    @Test
+    public void sufficientCashAvailableInvalido() {
+        CashDispenser cd = new CashDispenser();
+        
+        assertTrue(cd.isSufficientCashAvailable(CASH_DISPENSER_INITIAL_COUNT * 20));
+        
+        assertFalse(cd.isSufficientCashAvailable((CASH_DISPENSER_INITIAL_COUNT + 1) * 20));
+        assertFalse(cd.isSufficientCashAvailable(600 * 20));
+        assertFalse(cd.isSufficientCashAvailable(1000 * 20));
+        assertFalse(cd.isSufficientCashAvailable(10000 * 20));
+        assertFalse(cd.isSufficientCashAvailable(100000 * 20));
+        assertFalse(cd.isSufficientCashAvailable(1000000 * 20));
+        assertFalse(cd.isSufficientCashAvailable(10000000 * 20));
+        assertFalse(cd.isSufficientCashAvailable(100000000 * 20));
+    }
+}
